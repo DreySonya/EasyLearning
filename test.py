2619018a7017c09e39b395273fa3fb02589ff007
DreySonya <sony.v.bun@gmail.com>
2024-03-01 18:34:30 +0300

    Create second step on bot
    CrEaTe

--- test.py
+++ test.py
@@ -3,15 +3,45 @@ from telebot import types # для указание типов
 
 bot = telebot.TeleBot('6516287318:AAGy7FRbmO3Tb0CoJpBHdrlBsXIlXISwkoE')
 
-@bot.message_handler()
-def info(message):
 
+@bot.message_handler(commands=['start'])
+def main(message):
+      
     markup1 = types.InlineKeyboardMarkup()
-    markup1.add(types.InlineKeyboardButton('Нажми на меня!', callback_data='start'))
+    markup1.add(types.InlineKeyboardButton('Продолжить...', callback_data='test'))
+
+    bot.send_message(message.chat.id,f'Приветствуем вас, {message.from_user.first_name},в нашей онлайн школе «Учиться просто». У нас вам будет учиться не только просто, но и интересно. Вы с легкостью усвоите новый материал, а также закрепите старый. Занятия проводятся по русскому, математике, физике, истории, веб-дизайну в програме figma и создание сайтов(HTML/CSS). Вы сможете сами выбрать преподавателя, удобное время и дату. Нам интересны предметы, который мы преподаём, а также создадим интерес у вас.', reply_markup=markup1)
 
 
-    if message.text.lower() == 'привет':
-        bot.send_message(message.chat.id, f'Привет, рад вас видеть, для того чтобы взаимодействовать со мной или записаться на урок, то вам нужно использовать кнопочки, которые указаны ниже под строкой ввода сообщение или если вы уже взаимодействовали со мной вам, то у вас есть кнопочки под текстом коорый я вам писал', reply_markup=markup1)
+@bot.callback_query_handler(func = lambda callback1: True)
+def callback_message(callback):
+    
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton('Записаться на пробный урок', callback_data='prob'))
+    markup.add(types.InlineKeyboardButton('Записаться на урок', callback_data='lesson'))
+    
+    # markup3 = types.InlineKeyboardMarkup()
+    # markup3.add(types.InlineKeyboardButton('Записаться на урок', callback_data='lesson'))
+    
+    if callback.data == 'test':
+       bot.send_message(callback.message.chat.id, f'В школе есть определенные правила: 1. Уважать свое время, а также время преподавателя. Если вы понимаете, что не сможете посетить занятия по какой-либо причине, предупредите об этом заранее. 2.  Не опаздывать на занятие. Время очень ценно.   3. Экологичное общение. Не используем не цензурную брань, не ругаемся, уважительно относимся к преподавателю.', reply_markup=markup)
+       
+@bot.callback_query_handler(func = lambda callback: True)
+def callback_message(callback):
+    if callback.data == 'prob':
+        bot.send_message(callback.message.chat.id, f'Пробный урок')
+    
+    
+bot.polling(none_stop=True)
+
+
+   
+   
+   
+   
+   
+ # markup1 = types.InlineKeyboardMarkup()
+ # markup1.add(types.InlineKeyboardButton('Нажми на меня!', callback_data='start'))
 
 
 # @bot.message_handler(commands=['start'])
@@ -25,22 +55,4 @@ def info(message):
 
 
 #     # bot.send_message(message.chat.id,f'Приветствуем вас, {message.from_user.first_name},в нашей онлайн школе «Учиться просто». У нас вам будет учиться не только просто, но и интересно. Вы с легкостью усвоите новый материал, а также закрепите старый. Занятия проводятся по русскому, математике, физике, истории, веб-дизайну в програме figma и создание сайтов(HTML/CSS). Вы сможете сами выбрать преподавателя, удобное время и дату. Нам интересны предметы, который мы преподаём, а также создадим интерес у вас.', reply_markup=markup)
-
-@bot.callback_query_handler(func = lambda callback: True)
-def callback_message1(callback1_message):
-
-    markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton('test', callback_data='test'))
-
-
-    if callback1_message.data == 'start':
-        bot.send_message(callback1_message.message.chat.id, f'Приветствуем вас ,в нашей онлайн школе «Учиться просто». У нас вам будет учиться не только просто, но и интересно. Вы с легкостью усвоите новый материал, а также закрепите старый. Занятия проводятся по русскому, математике, физике, истории, веб-дизайну в програме figma и создание сайтов(HTML/CSS). Вы сможете сами выбрать преподавателя, удобное время и дату. Нам интересны предметы, который мы преподаём, а также создадим интерес у вас.', reply_markup=markup)
-
-
-@bot.callback_query_handler(func = lambda callback: True)
-def callback_message(callback):
-    if callback.data == 'test':
-       bot.send_message(callback.message.chat.id, f'Succses' )
-
-bot.polling(none_stop=True)
 
